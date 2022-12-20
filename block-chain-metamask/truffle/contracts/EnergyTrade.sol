4f2e13fd64bcdc497dfe0b910124df542bf478ab
formegusto <formegusto@gmail.com>
2022-12-20 14:53:17 +0900

    energy trade smart contract

--- block-chain-metamask/truffle/contracts/EnergyTrade.sol
+++ block-chain-metamask/truffle/contracts/EnergyTrade.sol
@@ -1,29 +1,58 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+/*
+ 1eth의 가치로 자신의 사용량을
+ Energy Market에 올려 공유하는 smart contract
+*/
 contract EnergyTrade {
     struct Rate {
-        address seller;
+        address payable seller;
         uint256 usage;
     }
     Rate[] private rateBySeller;
     uint256 private totalUsage = 0;
 
+    event Usage(uint256 usage);
+
     /**
      regist
      사용량 등록 (only 1 ether rate)
      */
-    function regist(uint256 _usage) public {
-        rateBySeller.push(Rate(msg.sender, _usage));
+    function regist(address payable seller, uint256 _usage) public  {
+        rateBySeller.push(Rate(seller, _usage));
         totalUsage += _usage;
     }
 
     
     /**
+     getAvailableOfPurchase
+     등록된 전력 데이터 공유
     */
-    function getAvaliableOfPurchase() public view returns (Rate[] memory){
+    function getAvailableOfPurchase() public view returns (Rate[] memory){
         return rateBySeller;
     }
 
+    /**
+     총 액수 확인
+     */
+    function getTotalUsage() public view returns (uint256) {
+        return totalUsage;
+    }
 
+    /**
+     sell
+     전력량 구매
+     */
+    function sell(uint256 index) public payable {
+        address payable seller = rateBySeller[index].seller;
+        seller.transfer(msg.value);
+
+        uint256 usage = rateBySeller[index].usage;
+
+        totalUsage -= usage;
+        delete rateBySeller[index];
+
+        emit Usage(usage);
+    }
 }
--- block-chain-metamask/truffle/test/EnergyTradeTest.js
+++ block-chain-metamask/truffle/test/EnergyTradeTest.js
@@ -13,11 +13,39 @@ contract("EnergyTradeTest", async (accounts) => {
   });
 
   it("Regist Test", async () => {
-    await ins.regist(10, { from: accounts[0] });
+    await ins.regist(accounts[0], 10);
 
-    const result = await ins.getAvaliableOfPurchase();
+    const result = await ins.getAvailableOfPurchase();
     const registed = result[0];
 
     await expect(registed[1]).to.be.equal("10");
+
+    const total = await ins.getTotalUsage();
+    await expect(total).to.be.bignumber.equal("10");
+  });
+
+  /**
+   * 상황
+   * account 1이 등록 후,
+   * account 2가 이를 1ether를 보내서 사는 과정
+   */
+  it("Sell Test", async () => {
+    // total 30
+    await ins.regist(accounts[0], 10);
+    await ins.regist(accounts[2], 20);
+
+    const result = await ins.sell(0, {
+      from: accounts[1],
+      value: web3.utils.toWei("1", "ether"),
+    });
+    const sellUsage = result.logs[0].args[0];
+    await expect(sellUsage).to.be.bignumber.equal("10");
+
+    // 0으로 바꾸기 작업
+    const availableOfPurchase = await ins.getAvailableOfPurchase();
+    await expect(availableOfPurchase[0][1]).to.be.equal("0");
+
+    const total = await ins.getTotalUsage();
+    await expect(total).to.be.bignumber.equal("20");
   });
 });
